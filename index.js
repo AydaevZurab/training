3a5136b06c7f6d7bd6b2330979a3f005cc7d0786
grozny <h6umid@gmail.com>
2023-02-27 01:48:14 +0300

    связка-настройка mongoDB с сервером

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
 app.use(express.json())
 
 app.use(require('./routes'))
 
-app.listen(3000, function () {
-    console.log('Сервер запущен');
-});
+mongoose.connect(
+    'mongodb+srv://into:code@cluster0.qa2hzxf.mongodb.net/backendTrening?retryWrites=true&w=majority', () => {
+        
+    console.log('connected')
+        
+        app.listen(3000, function () {
+            console.log('Сервер запущен');
+        });
+    })
+
 
